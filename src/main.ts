6e359ea1c2ff0845b56990e3a5ace536748309c6
munteanudenis87 <munteanudenis87@gmail.com>
2026-07-25 12:02:25 +0200

    Milestone 2

--- src/main.ts
+++ src/main.ts
@@ -18,3 +18,30 @@ type Person = {
   biography: string,
   image: string,
 }
+
+//  Milestone 2
+//  Crea un type alias Actress che oltre a tutte le proprietà di Person, aggiunge le seguenti proprietà:
+
+//  most_famous_movies: una tuple di 3 stringhe
+//  awards: una stringa
+//  nationality: una stringa tra un insieme definito di valori.
+//  Le nazionalità accettate sono: American, British, Australian, Israeli-American, South African, French, Indian, Israeli, Spanish, South Korean, Chinese.
+
+type ActressNationality = 
+  | "American"
+  | "British"
+  | "Australian"
+  | "Israeli-American"
+  | "South African"
+  | "French"
+  | "Indian"
+  | "Israeli"
+  | "Spanish"
+  | "South Korean"
+  | "Chinese"
+
+type Actress = Person & {
+  most_famous_movies: [string, string, string],
+  awards: string,
+  nationality: ActressNationality,
+}
